3154f8f3600c9b35665c9dbae2e3c1c2b58a1807
Sinai161 <ashleygalvan161@yahoo.com>
2024-09-23 18:42:33 -0700

    added user name and date to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 const Header = () => {
-// const [date, setDate ] = useState(new Date())
+    const [date, setDate ] = useState(new Date())
+
  
 const user = () => {
     return(
@@ -9,18 +10,21 @@ const user = () => {
     </div>
     )
 }
+function currentDate() { 
+    return(
+        <div>
+            <h1>{date.toDateString()}</h1>
+        </div>
+    )
+}
     // let currentDate = new Date()
     // console.log(currentDate)
-// const currentDate = () => {
-//     return(
-//     <div>
-//         <h1>Current Date: {date.toDateString()}</h1>
-//         <h1>{setDate.toDateString()}</h1>
-//     </div>
-//     )
-// }
+
     return(
-        user()
+        <div>
+            <h1>{user()}</h1>
+            <h1>{currentDate()}</h1>
+        </div>
     )
 }
 
