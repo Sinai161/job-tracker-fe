0569c5cbdebee9fd2865c5520acc4c1007052f18
Sinai161 <ashleygalvan161@yahoo.com>
2024-09-23 18:12:45 -0700

    added roboto font from google to index.html

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
-import React from "react";
-function Header() {
-    return (
-        <header>Hey Ashley!</header>
+// import React, { useState } from "react";
+const Header = () => {
+// const [date, setDate ] = useState(new Date())
+ 
+const user = () => {
+    return(
+     <div>
+        <h1>Hi Ashley</h1>
+    </div>
+    )
+}
+    // let currentDate = new Date()
+    // console.log(currentDate)
+// const currentDate = () => {
+//     return(
+//     <div>
+//         <h1>Current Date: {date.toDateString()}</h1>
+//         <h1>{setDate.toDateString()}</h1>
+//     </div>
+//     )
+// }
+    return(
+        user()
     )
 }
 
