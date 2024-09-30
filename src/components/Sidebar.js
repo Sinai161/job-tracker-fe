518942484d0cf166b1fae1dcae5aee017e0e5c1c
Sinai161 <ashleygalvan161@yahoo.com>
2024-09-30 16:31:04 -0700

    added sidebar logo and selectable items, also created assets folder

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,13 @@
 const Sidebar = () => {
-    
+    return(
+    <div>
+        <img src="assets/370082334-f2a67fab-f222-4bf2-82c3-0ad258b65a21.png" alt="logo"/>
+        <h1>Job Tracker</h1>
+        <h3>Dashboard</h3>
+        <h3>Applications</h3>
+        <h4>Ashley Galvan</h4>
+    </div>
+    )
 }
+
+export default Sidebar
